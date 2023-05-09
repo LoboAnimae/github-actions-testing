68a5b51d366d9ce4532692993632a16a8e3056a5
Andrés Quan-Littow <andrese.quanl@gmail.com>
2023-05-09 13:34:11 -0600

    Actions

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-console.log(process.env.SOME_SECRET)
-console.log(process.env.ENV_VAR)
+console.log("Secret:", process.env.SOME_SECRET)
+console.log("ENV_VAR:", process.env.ENV_VAR)
+console.log("All Vars:", process.env)
