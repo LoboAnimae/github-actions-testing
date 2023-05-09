561750da046938ff953740446e32f1873839f5d7
Andrés Quan-Littow <andrese.quanl@gmail.com>
2023-05-09 13:35:03 -0600

    Actions

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,14 @@
-const path = require('path');
-const nodeExternals  = require('webpack-node-externals');
+const path = require("path")
+const nodeExternals = require("webpack-node-externals")
 
-const { NODE_ENV = 'development' } = process.env;
+const NODE_ENV = "development"
 module.exports = {
-  entry: './index.js',
+  entry: "./index.js",
   mode: NODE_ENV,
-  target: 'node',
+  target: "node",
   externals: [nodeExternals()],
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].bundle.js",
   },
-};
-
+}
